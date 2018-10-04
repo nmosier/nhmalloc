6edc6b6b99256e0e5aba2fe47b56761afc20b95e
searidang <searidang@searidangs-mbp.middlebury.edu>
2018-10-03 21:48:49 -0400

    added a bit more

--- mymalloc.c
+++ mymalloc.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include<Linked_List.c>
+#include <stdbool.h>
 void * mymalloc(size_t size);
 void * find_available_spot();
 
@@ -12,6 +12,7 @@ void * find_available_spot();
 //keep information about each malloc allocation
 struct node_{
     size_t size_chunk;
+    int free? = 0;  //4 bytes for an int?
     void * p_start;
     void * p_end;
     struct node * next;
@@ -36,46 +37,64 @@ int main (int argc, char* argv[]){
 }
 //to be implemented: where to store the nodes of the linked_list
  void * mymalloc (size_t size){
+  //find available spot in the heap
+  node * node_available = find_available_spot(head,size);
 
-   //if there is an empty spot of a compatible size
-   //because there was a call to myfree()
-   void * p_available;   //find available spot in the heap
+  node * end_node = find_end_node(node * head);
+  void * p_used_end = end_node -> p_end;  //find the end of the used memory
 
+   //if there is an empty spot of a compatible size because there was a call to myfree()
 
+   if (node_available != NULL){
+       node_available -> free? = 0;
+       node_available -> size_chunk=size;
+       return node_available -> p_start;
+   }
    //else if there is enough memory to grow up and not pass the breakpoint
-   node * end_node = find_end_node(node * head);
-   void * p_used_end = end_node -> p_end;  //find the end of the used memory
-
+   else if (size + p_used_end < sbrk(0)){
+      node * new_node = push_node_end(head, size, p_used_end)
+      return new_node -> p_start;
+   }
 
    //else there is not enough memory
    else if (size + p_used_end >= sbrk(0)){   //sbrk(0) returns the current breakpoint
       brk(sbrk(0)+Break_Increment_Size);     //set new the break point
-      node * new_node;
-      new_node -> size_chunk = size;
-      new_node -> p_end = p_used_end + size;
-      new_node -> p_start = p_used_end + 4;  //is 4 the next address?
-      push_node_end(head, new_node)
+      node * new_node = push_node_end(head, size, p_used_end)
       return new_node -> p_start;
     }
 }
 
-void * find_available_spot(size_t size){
-
+node * find_available_spot(node * head, size_t size){
+  node * current = head;
+  while(current != NULL){
+    if ((current -> size_chunk >= size) && current->free?){
+      return current;
+    }
+    current = current -> next;
+  }
+  return current;
 }
 
-void push_node_end(node * head, node * new_node){
+node * push_node_end(node * head, size_t size, void * p_used_end){
+  node * new_node;
+  new_node -> size_chunk = size;
+  new_node -> p_end = p_used_end + size;
+  new_node -> p_start = p_used_end + 4;  //is 4 the next address?
+
+  //traverse through the linked list
   node * current = head;
   while(current != NULL){
     current = current -> next;
   }
   current -> next = new_node;
   current -> next -> next = NULL;
+  return new_node
 }
 
-node*  find_end_node(node * head){
+node * find_end_node(node * head){
   node * current = head;
   while(current != NULL){
-    current = current -> next;
+     current = current -> next;
   }
   return current;
 }
