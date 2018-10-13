bf0f93c1981191f9762247744203d65bae372cc4
Nicholas Mosier <nmosier@middlebury.edu>
2018-10-13 16:39:30 -0400

    updated list header

--- list.h
+++ list.h
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+#ifndef __LIST_H
+#define __LIST_H
+
 typedef struct list_node_t_ {
    size_t size;     // size of memory block (not including struct header)
    int free;        // is the block free or mallocated?
@@ -12,6 +15,9 @@ typedef struct {
 } list_t;
 
 
+typedef list_node_t memblock_t;
+typedef list_t memblocks_t;
+
 void list_append(list_node_t *nodep, list_t *listp);
 void list_prepend(list_node_t *nodep, list_t *listp);
 void list_insert(list_node_t *nodep, list_t *listp);
@@ -21,3 +27,5 @@ void list_insertafter(list_node_t *new_elem, list_node_t *ref_elem, list_t *list
 void list_print(list_t *listp);
 void list_dump(FILE *stream, list_t *listp);
 list_node_t *list_validate(list_t *listp);
+
+#endif
