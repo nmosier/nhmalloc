602792c4956e3534c7e558221341e5c9fe7899cb
Nicholas Mosier <nmosier@middlebury.edu>
2018-10-13 16:40:24 -0400

    initial commit

---
+++ memblock.h
@@ -0,0 +1,24 @@
+#ifndef __MEMBLOCK_H
+#define __MEMBLOCK_H
+
+#define BTREE
+
+#ifdef BTREE
+#include "bintree.h"
+#else
+   #ifdef LIST
+   #include "list.h"
+   #endif
+#endif
+// memblocks operations
+void memblocks_init(memblocks_t *memblocks);
+
+// memblock operations
+void memblock_insert(void *begin_addr, void *end_addr, memblocks_t *memblocks);
+memblock_t *memblock_find(size_t size, memblocks_t *memblocks);
+int memblock_split(memblock_t *block, size_t size, memblocks_t *memblocks);
+void memblock_free(void *begin_addr);
+void memblock_allocate(void *begin_addr);
+int memblocks_validate(memblocks_t *memblocks);
+
+#endif
