94f99a1ee4e7e8c8c8b6cc80a00491d7cf9ddace
Nicholas Mosier <nmosier@middlebury.edu>
2018-10-14 14:22:44 -0400

    modified list functions to use memblock_t struct with miniaml list members

--- list.c
+++ list.c
@@ -1,6 +1,9 @@
 // singly linked list implementation used for mymalloc
 #include <stdio.h>
 #include <stdbool.h>
+#include <assert.h>
+#include <string.h>
+#include <unistd.h>
 
 #include "list.h"
 #include "memblock.h"
@@ -8,6 +11,16 @@
 #define COLOR_RED   "\x1b[31m"
 #define COLOR_GREEN "\x1b[32m"
 #define COLOR_RESET "\x1b[0m"
+
+
+#define LOG(str) write(STDERR_FILENO, str, strlen(str))
+
+char eprintf_buf[1000];
+#define eprintf(fmt, ...) sprintf(eprintf_buf, fmt, __VA_ARGS__),   \
+   LOG(eprintf_buf)
+
+
+#define DEBUG 1
 
 /* list_append()
  * 
@@ -18,85 +31,85 @@ void list_append(list_node_t *nodep, list_t *listp) {
    if (listp->front == NULL) {
       listp->front = listp->back = nodep;
    } else {
-      listp->back->tail = nodep;
+      list_node_t *back, *tail;
+      back = listp->back;
+
+      if (DEBUG) {
+         tail = (list_node_t *) ((char *) back + back->size + sizeof(list_node_t));
+         assert(tail == nodep);
+      }
+
+      nodep->prevp = back;
       listp->back = nodep;
    }
-   nodep->tail = NULL;
 }
 
+// never actually used
+/*
 void list_prepend(list_node_t *nodep, list_t *listp) {
    nodep->tail = listp->front;
    listp->front = nodep;
 }
+*/
 
-inline void list_insert(list_node_t *nodep, list_t *listp) {
-   list_prepend(nodep, listp);
-}
 
-/* list_minlwrbnd() -- find minimum element of list with lower bound
- * RETVAL: ptr to node if found; NULL if no such element found
- */
-list_node_t *list_minlwrbnd(size_t lwrbnd, list_t *listp) {
-   size_t minval;
-   list_node_t *minnode, *list_it;
-
-   minval = (size_t) -1; // maximum value of size_t
-   minnode = NULL;
-   
-   /* iterate thru list */
-   for (list_it = listp->front; list_it; list_it = list_it->tail) {
-      /* update min val if between lower bound and current min val */
-      if (list_it->free && list_it->size >= lwrbnd && list_it->size <= minval) {
-         minnode = list_it;
-         minval = list_it->size;
-      }
+void list_insert(list_node_t *nodep, list_t *listp) {
+   if (nodep > listp->back) {
+      /* append to list */
+      list_append(nodep, listp);
+   } else {
+      /* insert somewhere in middle of list */
+      list_node_t *tail = (list_node_t *) ((char *) nodep + nodep->size + sizeof(list_node_t));
+      tail->prevp = nodep;
    }
-
-   /* note: if minnode was never updated, that means no suitable element was
-    *       found, so returning NULL is exactly what we want
-    */
-   return minnode;
 }
 
+
+/*
 void list_insertafter(list_node_t *new_elem, list_node_t *ref_elem, list_t *listp) {
+
+
+   
    list_node_t *swap = ref_elem->tail;
    ref_elem->tail = new_elem;
    new_elem->tail = swap;
 
-   /* update back of list if needed */
+   // update back of list if needed
    if (listp->back == ref_elem) {
       listp->back = new_elem;
    }
 }
+*/
 
 
 void list_print(list_t *listp) {
+   list_node_t *list_it;
    int i = 0;
-   for (list_node_t *list_it = listp->front; list_it; list_it = list_it->tail) {
-      printf("%s%p: %-10zu"COLOR_RESET"  ->  ", list_it->free ? COLOR_GREEN : COLOR_RED,
+
+   eprintf("list @ %p\n", (void *) listp->front);
+   
+   /* prime the loop */
+   list_it = listp->front;
+   if (list_it == NULL) {
+      LOG("(nil)\n");
+      return;
+   }
+   eprintf("%s%p: %-10zu"COLOR_RESET"  ->  ",
+          list_it->free ? COLOR_GREEN : COLOR_RED,
+          (void *) list_it, list_it->size);
+   ++i;
+   do {
+      list_it = (list_node_t *) ((char *) list_it + list_it->size + sizeof(list_node_t));
+      eprintf("%s%p: %-10zu"COLOR_RESET"  ->  ",
+             list_it->free ? COLOR_GREEN : COLOR_RED,
              (void *) list_it, list_it->size);
-      if (i == 3) {
-         printf("\n");
-         i = 0;
-      } else {
-         ++i;
+      if (i % 3 == 0) {
+         LOG("\n");
       }
-   }
-   printf(COLOR_RESET"\n");
-}
+      ++i;
+   } while (list_it != listp->back);
 
-
-void list_dump(FILE *stream, list_t *listp) {
-   for (list_node_t *list_it = listp->front; list_it; list_it = list_it->tail) {
-      fprintf(stream, "%p %c: %zu bytes\n", (void *) (list_it + 1),
-              list_it->free ? '_' : 'X',
-              list_it->size);
-      if (!list_it->free) {
-         fwrite((char *) (list_it + 1), sizeof(char), list_it->size, stream);
-         fprintf(stream, "\n");
-      }
-      fprintf(stream, "\n");
-   }
+   LOG(COLOR_RESET"\n");
 }
 
 /* list_validate()
@@ -108,15 +121,18 @@ void list_dump(FILE *stream, list_t *listp) {
  * RETVAL: returns pointer to first node that violates above conditions; if list is
  *         valid, returns NULL
  */
+
+// TO BE FIXED
+/*
 list_node_t *list_validate(list_t *listp) {
    list_node_t *prev = listp->front;
 
-   /* make sure prev is valid node */
+   // make sure prev is valid node
    if (prev == NULL || prev->size == 0) {
       return prev;
    }
 
-   /* validate all remaining nodes */
+   // validate all remaining nodes
    for (list_node_t *list_it = prev->tail; list_it; prev = list_it, list_it = list_it->tail) {
          if (((char *) (prev + 1)) + prev->size != (char *) list_it) {
             return list_it;
@@ -127,69 +143,4 @@ list_node_t *list_validate(list_t *listp) {
 
    return NULL;
 }
-
-
-
-
-
-void memblocks_init(list_t *memblocks) {
-   memblocks->front = memblocks->back = NULL;
-}
-
-void memblock_insert(void *begin_addr, void *end_addr, list_t *memblocks) {
-   list_node_t *memblock_header;
-   
-   /* initialize memblock header starting at begin_addr */
-   memblock_header = (list_node_t *) begin_addr;
-   memblock_header->size = (char *) end_addr - (char *) begin_addr - sizeof(list_node_t);
-   memblock_header->free = true;
-
-   /* append to memblocks list */
-   list_append(memblock_header, memblocks);
-}
-
-list_node_t *memblock_find(size_t size, list_t *memblocks) {
-   return list_minlwrbnd(size, memblocks);
-}
-
-/* memblock_split
- * DESC: splits memory block into two different blocks, the first of size
- *       _size_ and the second of the remaining size
- * NOTE: will only split the memblock if both resulting blocks will be
- *       nonzero in size
- *
- */
-int memblock_split(list_node_t *block, size_t size, list_t *memblocks) {
-   size_t total_size = block->size;
-   size_t remaining_size;
-   list_node_t *block2;
-   
-   if (total_size <= size + sizeof(list_node_t)) {
-      /* do not split blocks */
-      return false;
-   }
-
-   /* split blocks */
-   remaining_size = total_size - size - sizeof(list_node_t);
-   block->size = size; // update size of first block
-   block2 = (list_node_t *) ((char *) (block + 1) + size);
-
-   /* initialize new block */
-   block2->size = remaining_size;
-   block2->free = block->free;
-
-   /* insert into memblocks list */
-   list_insertafter(block2, block, memblocks);
-
-   return true;
-}
-
-void memblock_free(void *begin_addr) {
-   list_node_t *block = ((list_node_t *) begin_addr) - 1;
-   block->free = true;
-}
-
-void memblock_allocate(void *begin_addr) {
-   list_node_t *block = ((list_node_t *) begin_addr) - 1;
-   block->free = false;
-}
+*/
