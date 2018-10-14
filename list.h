a3416105eb5e454cbaada23a93da3c6acd54f9a7
Nicholas Mosier <nmosier@middlebury.edu>
2018-10-14 14:23:17 -0400

    changed types & typedefs to conform to new memblock_t struct

--- list.h
+++ list.h
@@ -1,31 +1,17 @@
-#include <stdio.h>
-
 #ifndef __LIST_H
 #define __LIST_H
 
-typedef struct list_node_t_ {
-   size_t size;     // size of memory block (not including struct header)
-   int free;        // is the block free or mallocated?
-   struct list_node_t_ *tail; // next memory block in heap (ptr to header)
-} list_node_t;
+#include <stdio.h>
+#include "memblock.h"
 
-typedef struct {
-   list_node_t *front;
-   list_node_t *back;
-} list_t;
-
-
-typedef list_node_t memblock_t;
-typedef list_t memblocks_t;
+typedef memblock_t list_node_t;
+typedef memblocks_t list_t;
 
 void list_append(list_node_t *nodep, list_t *listp);
-void list_prepend(list_node_t *nodep, list_t *listp);
+//void list_prepend(list_node_t *nodep, list_t *listp);
 void list_insert(list_node_t *nodep, list_t *listp);
-list_node_t *list_minlwrbnd(size_t lwrbnd, list_t *listp);
-void list_insertafter(list_node_t *new_elem, list_node_t *ref_elem, list_t *listp);
 
 void list_print(list_t *listp);
-void list_dump(FILE *stream, list_t *listp);
-list_node_t *list_validate(list_t *listp);
+//list_node_t *list_validate(list_t *listp);
 
 #endif
