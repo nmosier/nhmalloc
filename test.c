9f7dfdb58a98ba15959318af59de996776b1a6c1
Nicholas Mosier <nmosier@middlebury.edu>
2018-10-14 14:23:56 -0400

    nothing important changed

--- test.c
+++ test.c
@@ -10,8 +10,6 @@
 char eprintf_buf[1000];
 #define eprintf(fmt, ...) sprintf(eprintf_buf, fmt, __VA_ARGS__), LOG(eprintf_buf)
 
-extern btree_t memblocks;
-
 #define COLOR_RED   "\x1b[31m"
 #define COLOR_GREEN "\x1b[32m"
 #define COLOR_RESET "\x1b[0m"
@@ -19,12 +17,23 @@ extern btree_t memblocks;
 #define SEED  100
 
 int main(int argc, char *argv[]) {
-   const int iterations = 10;
+   const int iterations = 3;
    const char *base = "assfdghjggbfvdcfvgbfvdcfgbhnbgfvcdfgbbsrcgbvfcdvbhgtrdcvbhtrdvbhgtrdcfvgbhgtfdxcvbhgfdcvbnhgfvbhgfcvbhgfcvbnhgfcvbnjhgrdesrtyuytrewert";
    const size_t malloc_max = strlen(base);
    char *strs[100];
    size_t strlens[100];
 
+
+   void *ptrs[iterations];
+   for (int i = 0; i < iterations; ++i) {
+      ptrs[i] = malloc(i+1);
+   }
+   for (int i = 0; i < iterations; ++i) {
+      free(ptrs[i]);
+      //            memblocks_print(&memblocks);
+   }
+
+   return 0;
 
    // srandom(time(0));
    srandom(SEED);
@@ -33,23 +42,22 @@ int main(int argc, char *argv[]) {
       strlens[i] = (random() % malloc_max) + 1;
       strs[i] = malloc(strlens[i]);
       sprintf(strs[i], "%.*s", (int) strlens[i] - 1, base);
-      btree_print(memblocks);
    }
 
    for (int i = 0; i < iterations; ++i) {
       eprintf("%s\n", strs[i]);
       free(strs[i]);
-      btree_print(memblocks);
+      //      btree_print(memblocks);
    }
 
-   memblocks_validate(&memblocks);
-   
+   //   memblocks_validate(&memblocks);
+   exit(0);
    //   srandom(SEED);
    for (int i = 0; i < iterations; ++i) {
       strlens[i] = (random() % malloc_max);
       strs[i] = malloc(strlens[i]);
       sprintf(strs[i], "%.*s", (int) strlens[i] - 1, base);
-      btree_print(memblocks);
+      //      btree_print(memblocks);
    }
 
    return 0;
