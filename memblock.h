71bbaf4219a5ae123aece44b9afa196433705c56
Nicholas Mosier <nmosier@middlebury.edu>
2018-10-18 08:35:36 -0400

    added memblock_merge

--- memblock.h
+++ memblock.h
@@ -35,6 +35,12 @@ void memblocks_init(memblocks_t *memblocks);
 void memblock_insert(void *begin_addr, void *end_addr, memblocks_t *memblocks);
 memblock_t *memblock_find(size_t size, memblocks_t *memblocks);
 int memblock_split(memblock_t *block, size_t size, memblocks_t *memblocks);
+
+
+#define MEMBLOCK_MERGE_PREV 0x1
+#define MEMBLOCK_MERGE_NEXT 0x2
+memblock_t *memblock_merge(memblock_t *block, int direction, memblocks_t *memblocks);
+
 void memblock_free(void *begin_addr, memblocks_t *memblocks);
 void memblock_allocate(void *begin_addr, memblocks_t *memblocks);
 void memblocks_print(memblocks_t *memblocks);
