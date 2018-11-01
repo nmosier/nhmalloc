ae875053e2312214d7c3ac79705016a581a903f1
Nicholas Mosier <nmosier@middlebury.edu>
2018-11-01 13:31:24 -0400

    merged validation macros

---
+++ my-malloc.h
@@ -0,0 +1,17 @@
+// my-malloc.h
+// contains macros for memory alignment in malloc implementation
+
+#ifndef __MYMALLOC_H
+#define __MYMALLOC_H
+
+#include <stddef.h>
+
+#define MALLOC_ALIGN 16
+#define MALLOC_LOG_ALIGN 4
+#define MALLOC_ALIGN_MASK (~0 << MALLOC_LOG_ALIGN)
+#define MALLOC_ALIGN_SIZE(size) (((size-1) & MALLOC_ALIGN_MASK) \
+                                 + MALLOC_ALIGN)
+#define MALLOC_ALIGN_PTR(ptr) ((void *) MALLOC_ALIGN_SIZE((uint64_t) ptr))
+#define MALLOC_ALIGN_VALIDATE(val) (!((size_t) val & ~MALLOC_ALIGN_MASK))
+
+#endif
